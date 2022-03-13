d84c52c872b06834a878d49214dbc0de3782a4c0
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-03-13 16:36:00 +0530

    Create Write Concise Declarative Functions with ES6.js

---
+++ Write Concise Declarative Functions with ES6.js
@@ -0,0 +1,22 @@
+const bicycle = {
+  gear: 2,
+  setGear: function(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+// top code same to below code 
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
